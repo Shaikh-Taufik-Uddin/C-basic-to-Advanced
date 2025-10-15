886dc091b26a99c90ea34809c1abffc53854e3f7
Shaikh Taufik Uddin <shaikhtaufiku@gmail.com>
2025-10-15 21:05:13 +0530

    Add files via upload

---
+++ Day 9. Strings/caution.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    float b;
+    char c;
+    char str[100];
+    gets(str); // gets has to be used first of everything else because gets() cannot clear buffer value . else we have to use while(getchar()!='\0'); condition to clear buffer value
+    printf("Enter a: ");
+    scanf("%d", &a);
+    printf("Enter b: ");
+    scanf("%f", &b);
+    printf("Enter c: ");
+    scanf(" %c", &c); // the space is needed here to clear the input buffer ]
+    puts(str);
+    printf("%d", a);
+    printf("\n%f", b);
+    printf("\n%c", c);
+    return 0;
+}
